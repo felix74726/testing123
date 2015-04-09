313818edb15200c7d8b06aebbba40a12c1ece9e5
felix74726 <tadbilby@gmail.com>
2015-04-09 16:53:49 -0400

    this is a simple hello world style program
    prints hello and also does a getchar()

---
+++ hi.c
@@ -0,0 +1,22 @@
+// hi-c-test.cpp : Defines the entry point for the console application.
+//
+/* using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; */
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+		printf("\n hello print \n");
+		puts("\n hello string \n");
+		getchar();
+//		gets();
+            // Keep the console window open in debug mode.
+//            Console.WriteLine("Press any key to exit.");
+//            Console.ReadKey();
+
+	return 0;
+}
+
